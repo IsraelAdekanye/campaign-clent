6f10d3e572f6abb1524b94ea081ed0fc610a0314
IsraelMacBook <israeladedigba@yahoo.com>
2022-12-06 06:04:24 +0100

    nil

--- src/components/Donate/DonateElements.jsx
+++ src/components/Donate/DonateElements.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Row = styled.div`
-
+    padding: 3rem 0rem 3rem 0rem;
 `;
 
 export const Title = styled.p`
@@ -20,5 +20,13 @@ export const Label = styled.label`
 `;
 
 export const Submit = styled.button`
-
+padding: 0.6rem 1rem;
+border: none;
+background: #00b300;
+color: #fff;
+border-radius: 0.5rem;
+transition: 0.2s ease-out;
+margin-right: 2rem;
+font-size: 1rem;
+font-weight: lighter;
 `;
