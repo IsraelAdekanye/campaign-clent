1f0fed2c2580707e727f665eeea3553cf5a3ebb9
IsraelMacBook <israeladedigba@yahoo.com>
2022-12-06 06:45:58 +0100

    nil

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -65,7 +65,6 @@ const Donate = () => {
 
             </InDiv>
             
-
             <Submit type="submit">Donate</Submit>
 
           </DonationForm>
--- src/components/Donate/DonateElements.jsx
+++ src/components/Donate/DonateElements.jsx
@@ -2,38 +2,45 @@ import styled from 'styled-components';
 
 export const Row = styled.div`
     padding: 3rem 0rem 3rem 0rem;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const Title = styled.p`
-
+    justify-content: center;
+    font-size: clamp(3rem, 3vw, 3rem);
+	color: #072886;
 `;
 
 export const DonationForm = styled.form`
-
+justify-content: center;
 `;
 
 export const InDiv = styled.form`
     display: flex;
     flex-direction: row;
-    margin: 1rem 0rem 1rem 0rem
+    margin: 1rem 0rem 1rem 0rem;
+    justify-content: center;
+    justify-self: stretch;
 `;
 
 export const InnerDiv = styled.div`
-
+    margin: 0rem 1rem 0rem 1rem
 `;
 
 export const Label = styled.label`
-
+    margin: 0rem 1rem 0rem 1rem
 `;
 
 export const Submit = styled.button`
-//margin-top: 2rem 0rem 0rem 0rem
 padding: 0.6rem 1rem;
 border: none;
-background: #00b300;
+background: #072886;
 color: #fff;
 border-radius: 0.5rem;
 transition: 0.2s ease-out;
 font-size: 1rem;
 font-weight: lighter;
+align-self: center;
 `;
