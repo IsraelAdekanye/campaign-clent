fb8d9e751476ebb40097195cda66c6aff2443f45
IsraelMacBook <israeladedigba@yahoo.com>
2022-12-13 06:50:31 +0100

    added payment options

---
+++ src/components/PaymentOption/PaymentOptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MasterDiv, InDiv } from './PaymentOptionsElements';
+
+const PaymentOptions = () => {
+    return (
+        <MasterDiv>
+            
+        </MasterDiv>
+    )
+}
+
+export default PaymentOptions;
---
+++ src/components/PaymentOption/PaymentOptionsElements.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MasterDiv = styled.div`
+    max-width: 100%;
+    background: #f4f7f8;
+    border-radius: 8px;
+`;
