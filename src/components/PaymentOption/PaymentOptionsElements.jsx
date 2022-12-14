b6f472f58e5fafb91dad8b55cbee714b34755783
IsraelMacBook <israeladedigba@yahoo.com>
2022-12-14 18:11:09 +0100

    added payment options

--- src/components/PaymentOption/PaymentOptionsElements.jsx
+++ src/components/PaymentOption/PaymentOptionsElements.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const MasterDiv = styled.div`
     background-color: #f9b700;
+	background-image: url(${FeaturePic});
     max-width: 100%;
     background: #f4f7f8;
     border-radius: 8px;
