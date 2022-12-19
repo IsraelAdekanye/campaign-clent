95fb09fc767606287ef8bf506b0d39290e80d658
IsraelMacBook <israeladedigba@yahoo.com>
2022-12-19 17:55:40 +0100

    added account details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import SPA from './components/SPA';
 import SuccessPage from './components/Success/Success';
 import PaymentOptions from './components/PaymentOption/PaymentOptions';
+import Account from './components/AccountDetails/AccountDetails';
 
 const App =()=> {
 
@@ -14,6 +15,7 @@ const App =()=> {
           <Route path="/" element={ <SPA/> } />
 		      <Route path="/success" element={ <SuccessPage/> } />
           <Route path="/PaymentOptions" element={ <PaymentOptions/> } />
+          <Route path="/Account" element={ <Account/> } />
         </Routes>
       </BrowserRouter>
     </div>
