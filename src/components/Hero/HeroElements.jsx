4c9ab620f550b8e6ed8d49a7b3889437459dfa9e
IsraelMacBook <israeladedigba@yahoo.com>
2023-01-16 18:04:36 +0100

    added account details

--- src/components/Hero/HeroElements.jsx
+++ src/components/Hero/HeroElements.jsx
@@ -7,7 +7,7 @@ export const HeroContainer = styled.div`
 	//background-image: url(${ImgBg});
 	// height: 100vh;
 	// background-position: center;
-	// background-size: c;
+	// background-size: ;
 `;
 
 export const HeroContent = styled.div`
