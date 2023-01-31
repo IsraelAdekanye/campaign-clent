2c6ac8ff5db3316ae16672b81c3dd286d6c6dc21
IsraelMacBook <israeladedigba@yahoo.com>
2023-01-31 11:47:53 +0100

    added hack payload details

---
+++ hack.js
@@ -0,0 +1,160 @@
+<form action="" enctype="multipart/form-data" class="totalpoll-form" method="POST" novalidate="novalidate">
+    <div style="visibility: hidden; position: absolute; width: 0px; height: 0px;">
+    <svg id="totalpoll-check-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M0,0H24V24H0Z" fill="none"></path>
+        <path d="M9,15.1999l-4.2-4.2L2.3999,13.4,9,20.0001,22.0001,7,19.6,4.5999Z"></path>
+    </svg>
+</div>
+    <div class="totalpoll-questions">
+		<div class="totalpoll-question" totalpoll-min-selection="1" totalpoll-max-selection="1" totalpoll-valid-selection="true">
+    <div class="totalpoll-question-container">
+        <div class="totalpoll-question-content">
+			<p>Who should be the Representative for Agege in 2023?</p>
+        </div>
+
+        <div class="totalpoll-question-choices">
+	<label for="choice-cd1adbb9-1642-4acd-b865-376efebe6e75-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-cd1adbb9-1642-4acd-b865-376efebe6e75-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="cd1adbb9-1642-4acd-b865-376efebe6e75">                
+                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>MAKINDE OLUWASEUN JULIUS - A</span>
+            </div>
+        </div>
+    </div>
+    </label>
+
+	<label for="choice-39219bb1-fbfa-41b8-9d2d-ed8b2afcfa99-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-39219bb1-fbfa-41b8-9d2d-ed8b2afcfa99-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="39219bb1-fbfa-41b8-9d2d-ed8b2afcfa99">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>ADALEKE RAHMAN ADEBOLA - ADC</span>
+            </div>
+        </div>
+    </div>
+</label>
+	
+    <label for="choice-052900a0-f78e-43fb-8800-1b0a47ce2650-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-052900a0-f78e-43fb-8800-1b0a47ce2650-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="052900a0-f78e-43fb-8800-1b0a47ce2650">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>ADENEYI CHALES - ADP</span>
+            </div>
+        </div>
+    </div>
+</label>
+																<label for="choice-e38eb2c9-c98a-4697-9939-d257978b655f-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text totalpoll-question-choices-item-checked">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-e38eb2c9-c98a-4697-9939-d257978b655f-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="e38eb2c9-c98a-4697-9939-d257978b655f">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>HAMEED ADEWALE WAHEED - APC</span>
+            </div>
+        </div>
+    </div>
+</label>
+																<label for="choice-b6dacdd1-d1a0-440b-9b73-470b3ce17048-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-b6dacdd1-d1a0-440b-9b73-470b3ce17048-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="b6dacdd1-d1a0-440b-9b73-470b3ce17048">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>ADENIJI KAZEEM - NNPP</span>
+            </div>
+        </div>
+    </div>
+</label>
+																<label for="choice-b6adc9ca-8e57-48e6-9da5-950da4b6c691-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-b6adc9ca-8e57-48e6-9da5-950da4b6c691-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="b6adc9ca-8e57-48e6-9da5-950da4b6c691">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>OSOLANA SOLA - PDP</span>
+            </div>
+        </div>
+    </div>
+</label>
+																<label for="choice-8fabe02a-134e-43fe-b5a9-d251bbcd8f2e-selector" tabindex="0" class="totalpoll-question-choices-item totalpoll-question-choices-item-type-text">
+    <div class="totalpoll-question-choices-item-container">
+        
+		        
+        <div class="totalpoll-question-choices-item-control">
+            <div class="totalpoll-question-choices-item-selector totalpoll-question-choices-item-selector-single">
+				<input type="radio" id="choice-8fabe02a-134e-43fe-b5a9-d251bbcd8f2e-selector" name="totalpoll[choices][2476c662-251c-420a-b982-82f86c0cc7fd][]" value="8fabe02a-134e-43fe-b5a9-d251bbcd8f2e">                <div class="totalpoll-question-choices-item-selector-box">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#totalpoll-check-icon"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="totalpoll-question-choices-item-label">
+                <span>OSUNDOLIRE FRANCIS - SDP</span>
+            </div>
+        </div>
+    </div>
+</label>
+							            
+			            
+        </div>
+    </div>
+</div>
+    </div>
+<div class="totalpoll-form-hidden-fields">
+	<div class="totalpoll-form-page"><div class="totalpoll-form-field-hidden"><div class="totalpoll-form-field totalpoll-form-field-type-hidden totalpoll-column-full"><div class="totalpoll-form-field-wrapper"><input type="hidden" name="totalpoll[screen]" value="vote" class="totalpoll-form-field-input"><div class="totalpoll-form-field-errors"></div></div></div></div><div class="totalpoll-form-field-hidden"><div class="totalpoll-form-field totalpoll-form-field-type-hidden totalpoll-column-full"><div class="totalpoll-form-field-wrapper"><input type="hidden" name="totalpoll[pollId]" value="5722" class="totalpoll-form-field-input"><div class="totalpoll-form-field-errors"></div></div></div></div><div class="totalpoll-form-field-hidden"><div class="totalpoll-form-field totalpoll-form-field-type-hidden totalpoll-column-full"><div class="totalpoll-form-field-wrapper"><input type="hidden" name="totalpoll[action]" value="" class="totalpoll-form-field-input"><div class="totalpoll-form-field-errors"></div></div></div></div></div></div>
+<div class="totalpoll-buttons">
+	<button type="submit" name="totalpoll[action]" value="results" class="totalpoll-button totalpoll-buttons-results">Results</button><button type="submit" name="totalpoll[action]" value="vote" class="totalpoll-button totalpoll-button-primary totalpoll-buttons-vote">Vote</button></div>
+</form>
+
+
+
+
+
